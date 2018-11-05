5d4c12a1b9b4bfae06be02a039af63f9cdf9f58f
balu4007 <balu.ramu1@gmail.com>
2018-11-05 08:12:51 +0530

    updated format suffix patterns

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path,include
-from .views import snippet_details,snippet_list
+from django.urls import path, include
+from rest_framework.urlpatterns import format_suffix_patterns
 
+from .views import snippet_details, snippet_list
 
-urlpatterns=[
+urlpatterns = [
     path('snippets/', snippet_list),
     path('snippets/<int:pk>', snippet_details),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
